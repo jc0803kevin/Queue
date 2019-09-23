3f4e02160050f37ef6ddcb11124e309ed0fc15b4
jc0803kevin <jc0803kevin@163.com>
2019-09-23 17:23:41 +0800

    kafka

---
+++ springboot-kafka/src/main/java/com/kevin/mq/springbootkafka/ctrl/TestKafkaProducerController.java
@@ -0,0 +1,27 @@
+package com.kevin.mq.springbootkafka.ctrl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author kevin
+ * @Description
+ * @Date Created on 2019/9/19 21:46
+ */
+
+@RestController
+@RequestMapping("/kafka")
+public class TestKafkaProducerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("/send")
+    public String send(String message){
+        kafkaTemplate.send("test_topic", message);
+        return message;
+    }
+
+}
