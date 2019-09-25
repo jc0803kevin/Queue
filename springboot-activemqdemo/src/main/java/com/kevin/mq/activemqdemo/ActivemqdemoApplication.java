c516b3c1d0fe3243236028eacc5e82b2e361a9cf
jc0803kevin <jc0803kevin@163.com>
2019-09-25 16:53:39 +0800

    activemq

--- springboot-activemqdemo/src/main/java/com/kevin/mq/activemqdemo/ActivemqdemoApplication.java
+++ springboot-activemqdemo/src/main/java/com/kevin/mq/activemqdemo/ActivemqdemoApplication.java
@@ -1,7 +1,7 @@
 package com.kevin.mq.activemqdemo;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActivemqdemoApplication {
