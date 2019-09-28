7a2049d3ba28005d6bd3f62cc170d5df9a25f42d
jc0803kevin <jc0803kevin@163.com>
2019-09-28 09:06:11 +0800

    自定义队列

---
+++ cusqueue/src/main/java/com/kevin/mq/cusqueue/CusqueueApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.mq.cusqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusqueueApplication.class, args);
+	}
+
+}
---
+++ cusqueue/src/test/java/com/kevin/mq/cusqueue/CusqueueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kevin.mq.cusqueue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CusqueueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
