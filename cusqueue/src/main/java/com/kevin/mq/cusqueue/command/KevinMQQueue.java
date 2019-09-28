d072651db94506a991cea642f6cc0282a5e048a8
jc0803kevin <jc0803kevin@163.com>
2019-09-28 09:24:07 +0800

    自定义队列

---
+++ cusqueue/src/main/java/com/kevin/mq/cusqueue/command/KevinMQQueue.java
@@ -0,0 +1,9 @@
+package com.kevin.mq.cusqueue.command;
+
+/**
+ * @Author kevin
+ * @Description 队列
+ * @Date Created on 2019/9/28 9:22
+ */
+public class KevinMQQueue {
+}
--- springboot-activemqdemo/src/main/java/com/kevin/mq/activemqdemo/config/ActiveMQConfig.java
+++ springboot-activemqdemo/src/main/java/com/kevin/mq/activemqdemo/config/ActiveMQConfig.java
@@ -3,7 +3,6 @@ package com.kevin.mq.activemqdemo.config;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
