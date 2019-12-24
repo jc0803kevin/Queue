5434d3f46c9a013410cce2b53412a736fab596f2
jc0803kevin <jc0803kevin@163.com>
2019-12-24 14:43:32 +0800

    readme

--- sprintboot-rocketmq/src/main/java/com/kevin/mq/sprintbootrocketmq/SprintbootRocketmqApplication.java
+++ sprintboot-rocketmq/src/main/java/com/kevin/mq/sprintbootrocketmq/SprintbootRocketmqApplication.java
@@ -3,6 +3,12 @@ package com.kevin.mq.sprintbootrocketmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author kevin
+ * @Description 启动类
+ * @Date Created on 2019/12/24 14:43
+ */
+
 @SpringBootApplication
 public class SprintbootRocketmqApplication {
 
